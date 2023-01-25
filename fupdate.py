76ffb26374c073f3cf4b3d0bb972f9eaae3c1acf
ItsIgnacioPortal <5990@protonmail.com>
2023-01-25 04:37:16 -0300

    initial commit

---
+++ fupdate.py
@@ -0,0 +1,206 @@
+import os
+import re
+import semver
+import requests
+import json
+from termcolor import colored
+from urllib.parse import urlparse
+
+
+devMode = True
+
+
+def error(message):
+	print(colored("ERROR: ", "red") + message)
+
+def warning(message):
+	print(colored("WARNING: ", "yellow") + message)
+
+def getGithubChangelog(url):
+	if githubToken != "":
+		headers = {"Accept": "application/vnd.github+json", "Authorization": "Bearer " + githubToken, "X-GitHub-Api-Version": "2022-11-28"}
+		
+		#TODO: Error handling and throttling
+		response = requests.get(url, headers=headers)
+
+		responseJSON = json.loads(response.text)
+
+		try:
+			return responseJSON["body"]
+		except:
+			return colored("ERROR: ", "red") + "This version does not exist: " + colored(url,"yellow")
+
+def getPypiChangelog(package):
+	url = "https://pypi.org/pypi/" + package + "/json"
+
+	#TODO: Error handling and throttling
+	response = requests.get(url)
+	responseJSON = json.loads(response.text)
+
+	if response.status_code != 200:
+		error("Pypi API error. Got status code " + colored(str(response.status_code), "yellow") + " for URL " + colored(url, "yellow"))
+	else:
+		try:
+			sourceCodeURL = responseJSON["info"]["project_urls"]["Source"]
+			sourceCodeURL = urlparse(sourceCodeURL)
+			if sourceCodeURL.hostname == "github.com":				
+				pathList = (sourceCodeURL.path[1:]).split("/") 
+				pathListLen = len(pathList)
+
+				#Normally pathListLen would always be equal to 2, but in the rare case where someone put the URL as (for example) "https://github.com/username/repo/", the len will be three, because of that extra slash at the end. This is also done to prevent potential CSRF or token leaks
+				if pathListLen == 2 or pathListLen == 3:
+					# url = "https://api." + packageList[0] + "/repos/" + packageList[1] + "/" + packageList[2] + "/releases/tags/v" + newVersion
+					url = "https://api.github.com/repos/" + pathList[0] + "/" + pathList[1] + "/releases/tags/v" + newVersion
+
+					return getGithubChangelog(url)
+
+				else:
+					return colored("FATAL ERROR: ", "red") + "The github source code URL for " + colored(package, "yellow") + " was malformed: " + colored(sourceCodeURL, "yellow")
+
+			else:
+				return warning("Unable to fetch changelog for " + colored(package, "yellow") + ". The source code was not hosted on github.")
+
+		except KeyError:
+			#TODO: Add an option to allow the user to fill in the source code site
+			return warning("Unable to get source code site for the " + colored(package, "yellow") + " pypi package.")
+
+"""
+goto :comment
+1. Get list of all outdated packages
+2. If a package has upgraded a major version number, show changelog
+3. "Do you want to updated X packages? [Y/n]: "
+4. Update everything
+"""
+
+try:
+	githubToken = os.environ["fupdate-github-token"]
+except KeyError:
+	warning("No github token detected. Please set the environment variable " + colored("fupdate-github-token") + " to your github personal access token. Without it, we can't fetch the changelogs.")
+	githubToken = ""
+
+if not devMode:
+	stream = os.popen("gup check")
+	gupOutput = stream.readlines()
+else:
+	gupOutput=["gup: INFO: check binary under $GOPATH/bin or $GOBIN",
+	"gup: INFO: [1/6] github.com/gwen001/github-subdomains (Already up-to-date: v1.2.0)",
+	"gup: INFO: [2/6] github.com/OJ/gobuster/v3 (Already up-to-date: v3.4.0)"
+	"gup: INFO: [3/6] github.com/nao1215/gup (Already up-to-date: v0.15.1)",
+	"gup: INFO: [4/6] github.com/j3ssie/metabigor (Already up-to-date: v1.12.1)",
+	"gup: INFO: [5/6] github.com/ossf/criticality_score (Already up-to-date: v1.0.7)",
+	"gup: INFO: [6/6] github.com/itsignacioportal/hacker-scoper (v1.0.0 to v3.0.0)"]
+
+packages = []
+for line in gupOutput:
+	line = line.strip()
+	if line != "gup: INFO: check binary under $GOPATH/bin or $GOBIN":
+		if "Already up-to-date" not in line:
+			line = re.sub(r".*\[[0-9]*\/[0-9]*\] ", "", line)
+
+			packagelist = re.findall(r".+ \(", line)
+			package = packagelist[0]
+			package = package[:-2]
+			packages.append(package)
+
+			versionList = re.findall(r"\(.*\)", line)
+			newVersion = ((re.findall(r"to .*", versionList[0]))[0])[3:-1]
+			oldVersion = ((re.findall(r".* to", versionList[0]))[0])[1:-3]
+			
+			if newVersion.startswith("v"):
+				newVersion = newVersion[1:]
+
+			if oldVersion.startswith("v"):
+				oldVersion = oldVersion[1:]
+
+			semverNewVersion = semver.VersionInfo.parse(newVersion)
+			semverOldVersion = semver.VersionInfo.parse(oldVersion)
+
+			if(semverNewVersion.major > semverOldVersion.major):
+				print(colored("NEW MAJOR VERSION: ", "green") + colored("(gup) ", "yellow") + line)
+				if package.startswith("github.com"):
+					packageList = package.split("/")
+					
+					if not devMode:
+						url = "https://api." + packageList[0] + "/repos/" + packageList[1] + "/" + packageList[2] + "/releases/tags/v" + newVersion
+
+						print(getGithubChangelog(url) + "\n")
+
+				else:
+					print("You must manually check the release notes for: " + package)
+
+if not devMode:
+	stream = os.popen("pip list --outdated")
+	pipOutput = stream.readlines()
+else:
+	pipOutput = ["Package    Version Latest Type",
+	"---------- ------- ------ -----",
+	"pip_audit  1.1.2  2.4.14 wheel",
+	"safety     2.4.0  2.5.0   wheel",
+	"guessit    2.5.0  3.0.0   wheel",
+	"rich       13.0.1  13.2.0 wheel",
+	"setuptools 65.5.0  66.1.1 wheel"]
+
+pipWhitelistedPackages = ["pip_audit",
+"safety",
+"guessit",
+"srt"]
+
+pipPackagesToUpdate=[]
+
+pipFirstLoop = True
+for line in pipOutput[2:]:
+	package = re.findall(r"^([^\s]+)", line)
+	package = package[0]
+
+	if package in pipWhitelistedPackages:
+	
+		# Regex magic
+		oldVersion = re.sub(package + "( )+", "", line)
+		newVersion = oldVersion
+		oldVersion = re.findall("^([^\s]+)", oldVersion)
+		newVersion = re.sub(oldVersion[0] + "( )+", "", newVersion)
+		newVersion = re.findall("^([^\s]+)", newVersion)
+
+		# List to string
+		newVersion = newVersion[0]
+		oldVersion = oldVersion[0]
+
+		semverNewVersion = semver.VersionInfo.parse(newVersion)
+		semverOldVersion = semver.VersionInfo.parse(oldVersion)
+
+		if(semverNewVersion > semverOldVersion):
+			pipPackagesToUpdate.append(package)
+
+			#TODO: Add flag to enable show changelog for major, minor and patch releases
+
+			if(semverNewVersion.major > semverOldVersion.major):
+				print(colored("NEW MAJOR VERSION: ", "green") + colored("(pip) ", "yellow") + package + " (" + oldVersion + " to " + newVersion + ")")
+
+				try:
+					changelog = getPypiChangelog(package)
+					print(changelog + "\n")
+				except:
+					# If getPypiChangelog returned an error...
+					continue
+
+			elif(semverNewVersion.minor > semverOldVersion.minor):
+				print(colored("New minor version: ", "blue") + colored("(pip) ", "yellow") + package + " (" + oldVersion + " to " + newVersion + ")")
+
+			else:
+				print("New patch version: " + colored("(pip) ", "yellow") + package + " (" + oldVersion + " to " + newVersion + ")")
+
+
+
+
+"""
+pip virtualenvs
+	cmd /k "cd C:\Program Files\HackingSoftware\safetyPythonVenv\Scripts & activate & pip install safety --upgrade & deactivate & exit 0"
+	cd C:\Program Files\HackingSoftware\github-search
+	git pull
+	cd C:\Program Files\HackingSoftware\graudit
+	git pull
+choco
+npm
+microsoft store
+winget
+"""
